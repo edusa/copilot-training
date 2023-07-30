a019d4a788793e54e81a0caf50909bf540288417
edusa <edulousa@gmail.com>
2023-07-30 18:10:21 +0100

    correction to member.js

--- member.js
+++ member.js
@@ -1,6 +1,3 @@
-function skillsMember()) {
-    console.log('I am a member of the skills.js file')
-}
-function skillsMember()) {
+function skillsMember() {
     console.log('I am a member of the skills.js file')
 }
